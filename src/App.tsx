b370b2f9a3bb9b4bdd4bab8a695df4a0e3d4df6a
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-08 09:47:35 -0600

    configure canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
-import "./App.css";
+import { useEffect, useRef } from "react";
+import { clearCanvas, setCanvasSize } from "./utils/canvasUtils";
+
+const WIDTH = 1024;
+const HEIGHT = 768;
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const getCanvasWithContext = (canvas = canvasRef.current) => {
+    return { canvas, context: canvas?.getContext("2d") };
+  };
+
+  useEffect(() => {
+    const { canvas, context } = getCanvasWithContext();
+
+    if (!canvas || !context) return;
+
+    setCanvasSize(canvas, WIDTH, HEIGHT);
+
+    context.lineJoin = "round";
+    context.lineCap = "round";
+    context.lineWidth = 5;
+    context.strokeStyle = "black";
+
+    clearCanvas(canvas);
+  }, []);
+
   return (
     <div className="window">
       <div className="title-bar">
@@ -9,6 +34,7 @@ function App() {
           <button arial-label="Close" />
         </div>
       </div>
+      <canvas ref={canvasRef} />
     </div>
   );
 }
