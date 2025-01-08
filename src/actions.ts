bfc04e6f669053d3089b364ef6dafe33adc61f6f
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-08 12:03:39 -0600

    add the undo and redo actions

--- src/actions.ts
+++ src/actions.ts
@@ -4,6 +4,8 @@ export const BEGIN_STROKE = "BEGIN_STROKE";
 export const UPDATE_STROKE = "UPDATE_STROKE";
 export const END_STROKE = "END_STROKE";
 export const SET_STROKE_COLOR = "SET_STROKE_COLOR";
+export const UNDO = "UNDO";
+export const REDO = "REDO";
 
 export type Action =
   | {
@@ -21,6 +23,12 @@ export type Action =
   | {
       type: typeof SET_STROKE_COLOR;
       payload: string;
+    }
+  | {
+      type: typeof UNDO;
+    }
+  | {
+      type: typeof REDO;
     };
 
 export const beginStroke = (x: number, y: number) => {
@@ -37,4 +45,12 @@ export const endStroke = () => {
 
 export const setStrokeColor = (color: string) => {
   return { type: SET_STROKE_COLOR, payload: color };
+};
+
+export const undo = () => {
+  return { type: UNDO };
+};
+
+export const redo = () => {
+  return { type: UNDO };
 };
--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -2,7 +2,9 @@ import {
   Action,
   BEGIN_STROKE,
   END_STROKE,
+  REDO,
   SET_STROKE_COLOR,
+  UNDO,
   UPDATE_STROKE,
 } from "./actions";
 import { RootState } from "./utils/types";
@@ -10,9 +12,12 @@ import { RootState } from "./utils/types";
 const initialState: RootState = {
   currentStroke: { points: [], color: "#000" },
   strokes: [],
+  historyIndex: 0,
 };
 
 export const currentStrokeSelector = (state: RootState) => state.currentStroke;
+export const historyIndexSelector = (state: RootState) => state.historyIndex;
+export const strokesSelector = (state: RootState) => state.strokes;
 
 export const rootReducer = (
   state: RootState = initialState,
@@ -40,10 +45,13 @@ export const rootReducer = (
     case END_STROKE: {
       if (!state.currentStroke.points.length) return state;
 
+      const historyIndex = state.strokes.length - state.historyIndex;
+
       return {
         ...state,
+        historyIndex: 0,
         currentStroke: { ...state.currentStroke, points: [] },
-        strokes: [...state.strokes, state.currentStroke],
+        strokes: [...state.strokes.slice(0, historyIndex), state.currentStroke],
       };
     }
     case SET_STROKE_COLOR: {
@@ -54,6 +62,17 @@ export const rootReducer = (
           ...{ color: action.payload },
         },
       };
+    }
+    case UNDO: {
+      const historyIndex = Math.min(
+        state.historyIndex + 1,
+        state.strokes.length,
+      );
+      return { ...state, historyIndex };
+    }
+    case REDO: {
+      const historyIndex = Math.max(state.historyIndex - 1, 0);
+      return { ...state, historyIndex };
     }
     default:
       return state;
