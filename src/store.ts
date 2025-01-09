c4b3288fa35fb6b7f6a8c8b0bc3d4604f2a15fec
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-09 13:07:36 -0600

    define modal slice

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,16 @@
 import logger from "redux-logger";
-// import { reducer as historyIndex } from "./modules/historyIndex/reducer";
 import { currentStroke } from "./modules/currentStroke/slice";
 import strokes from "./modules/strokes/slice";
 import { configureStore } from "@reduxjs/toolkit";
 import historyIndex from "./modules/historyIndex/slice";
+import { modalVisible } from "./modules/modals/slice";
 
 export const store = configureStore({
   reducer: {
     historyIndex,
     strokes,
     currentStroke,
+    modalVisible,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
